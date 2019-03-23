cebd11e283e9178ae5d6cf1ddd0e4fe5cbd0d3c2
tsofiya <47355819+tsofiya@users.noreply.github.com>
2019-03-23 21:25:56 +0200

    init Commit

---
+++ src/MatlabFunctions.py
@@ -0,0 +1,37 @@
+import numpy as np
+from scipy.signal import convolve2d
+
+
+def matlab_style_gauss2D(shape=(3, 3), sigma=0.5):
+    """
+    2D gaussian mask - should give the same result as MATLAB's
+    fspecial('gaussian',[shape],[sigma])
+    """
+    m, n = [(ss - 1.) / 2. for ss in shape]
+    y, x = np.ogrid[-m:m + 1, -n:n + 1]
+    h = np.exp(-(x * x + y * y) / (2. * sigma * sigma))
+    h[h < np.finfo(h.dtype).eps * h.max()] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    psfHeatmap = np.mat(h)
+    return psfHeatmap
+
+
+def conv2(x, y, mode='same'):
+    return np.rot90(convolve2d(np.rot90(x, 2), np.rot90(y, 2), mode=mode), 2)
+
+
+if __name__ == '__main__':
+    psfHeatmap = matlab_style_gauss2D([7, 7], 1)
+    for i in range(0, psfHeatmap.shape[0]):
+        for j in range(0, psfHeatmap.shape[1]):
+            print(round(psfHeatmap[i, j], 4), end='')
+            print(" ", end='')
+        print("\n")
+
+
+def ind2sub(arrayRow, ind):
+    rows = (ind / arrayRow)
+    cols = (ind % arrayRow)  # or numpy.mod(ind.astype('int'), array_shape[1])
+    return np.array([int(rows), int(cols)])
---
+++ src/main.py
@@ -0,0 +1,21 @@
+import argparse
+import numpy as np
+
+from createdata import CreateData
+from Training import train_model
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cameraPixelSize", type=int, help="Camera pixel size in [nm]")
+    parser.add_argument("Directory", type=str, help="Files directoy")
+    parser.add_argument("tifName", type=str, help="Images name")
+    parser.add_argument("csvFileName", type=str, help="csv name")
+    parser.add_argument("--upSamplingFactor", type=int, help="Ratio for upsampling", default=8)
+    parser.add_argument("--patchBeforeUpSample", type=int, help="Patch size", default=26)
+
+    args = parser.parse_args()
+
+    patches, heatmaps = CreateData(args.cameraPixelSize, args.Directory, args.tifName, args.csvFileName,
+                                   args.upSamplingFactor, args.patchBeforeUpSample)
+
+    train_model(np.concatenate(patches, axis=0), np.concatenate(heatmaps, axis=0), 'weights', 'meanstd')
